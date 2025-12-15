ce5dd43c0b0ad2a28c029644ea56cc57aa9ac8df
faroosa123 <104206380+faroosa123@users.noreply.github.com>
2025-12-15 20:32:01 +0200

    Fix PORT assignment to allow external access

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 0.0.0.0;
 
 app.get('/', (req, res) => {
   res.send('Bot is running!');
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
